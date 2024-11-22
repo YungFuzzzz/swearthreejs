aad8563dd0ad6958402caf004b506e470d128220
FUZZZ <147380081+YungFuzzzz@users.noreply.github.com>
2024-11-22 13:41:21 +0100

    feat: added barrel to display shoe

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -9,12 +11,28 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-camera.position.z = 5;
+const loader = new GLTFLoader();
+loader.load('/assets/models/barrel.glb', (gltf) => {
+    gltf.scene.position.y = 1.3;
+    scene.add(gltf.scene);
+}, undefined, (error) => {
+    console.error(error);
+});
+
+const planeGeometry = new THREE.PlaneGeometry(20, 20, 5);
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000, side: THREE.DoubleSide });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -Math.PI / 2;
+scene.add(plane);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 10);
+scene.add(ambientLight);
+
+camera.position.z = 2;
+camera.position.y = 2;
+camera.rotation.x = -0.7;
 
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -23,8 +41,5 @@ window.addEventListener('resize', () => {
 });
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
     renderer.render(scene, camera);
 }
